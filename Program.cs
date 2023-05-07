d7785fb1006fc5d7d7eb79d953c121a96c010c6a
Serhii Amelin <amserega90@gmail.com>
2023-05-07 20:17:48 +0300

    Starting project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SumEvent
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            ClassCounter counter = new ClassCounter();
+
+            counter.onCount += counter.SumTogether;
+            counter.onCount += counter.SumTogether;
+
+            counter.Sum(5, 10);
+        }
+    }
+}
