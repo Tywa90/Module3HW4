d3184ebb9d349aa6ea80297d1e91dde8f4697815
Serhii Amelin <amserega90@gmail.com>
2023-05-08 15:21:30 +0300

    Task2 completed

--- Customer.cs
+++ Customer.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace SumEvent
 {
-    public class Customer : IComparable<Customer>
+    public class Customer 
     {
         public int CustomerId { get; set; }
         public string Name { get; set; }
+        public int Age { get; set; }
         public string Adress { get; set; }
 
-        public Customer(int customerId, string name, string adress)
+        public Customer(int customerId, string name, int age, string adress)
         {
             CustomerId = customerId;
             Name = name;
+            Age = age;
             Adress = adress;
         }
 
-        public int CompareTo(Customer? other)
+        public override string ToString()
         {
-            return Name.CompareTo(other.Name);
+            return Name;
         }
     }
 }
--- Task2Linq.cs
+++ Task2Linq.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,47 +25,104 @@ namespace SumEvent
             AddOrder();
 
             var selected1 = from c in Customers
-                       where c.CustomerId > 5
-                       orderby c.CustomerId
-                       select c;
+                            where c.Age >= 18
+                            orderby c.CustomerId
+                            select c;
+
+            if (selected1.All(c => c.Age >= 18))
+            {
+                Console.WriteLine("Adults customers");
+            }
+            else
+            {
+                Console.WriteLine("Not all customers are adults");
+            }
+
             foreach (var customer in selected1)
             {
                 Console.WriteLine(customer.Name);
             }
 
+            Console.WriteLine("\nAll customers from Kyiv: ");
             var selected2 = from c in Customers
-                           where c.CustomerId > 5
-                           orderby c.Name
-                           select c.CustomerId + " " + c.Name;
+                            where c.Adress == "Kyiv"
+                            orderby c.Name descending
+                            select c.CustomerId + " " + c.Name + " " + c.Adress;
             foreach (var customer in selected2)
             {
                 Console.WriteLine(customer);
+            }
+
+            Console.WriteLine("\nFirst Customer in ages from 20 to 30 years: ");
+            var selected3 = Customers
+                .FirstOrDefault(c => c.Age > 20 && c.Age < 30);
+            Console.WriteLine(selected3);
+
+            Console.WriteLine("\nJoin tables Customer & Order:");
+            var result = Customers
+                .Join(Orders,
+                c => c.CustomerId,
+                ord => ord.CustomerId,
+                (c, ord) => new
+                {
+                    CustomerId = ord.CustomerId,
+                    Name = c.Name,
+                    OrderId = ord.OrderId,
+                    Quantity = ord.Quantity
+                })
+                .Where(c => c.Quantity > 10)
+                .OrderBy(o => o.OrderId);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"OrderID: {item.OrderId} Quantity: {item.Quantity} Customer: {item.Name} ");
+            }
+
+            Console.WriteLine("\nGroup orders by Customer name: ");
+            var groups = Customers
+                .Join(Orders,
+                c => c.CustomerId,
+                ord => ord.CustomerId,
+                (c, ord) => new
+                {
+                    CustomerId = ord.CustomerId,
+                    Name = c.Name,
+                    OrderId = ord.OrderId,
+                })
+                .GroupBy(c => c.Name);
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Key);
+                foreach (var customer in group)
+                {
+                    Console.WriteLine($"\t{customer.OrderId}");
+                }
             }
         }
 
         private void AddCustomer()
         {
-            Customers.Add(new Customer(1, "David", "Lviv"));
-            Customers.Add(new Customer(2, "Boris", "Kyiv"));
-            Customers.Add(new Customer(3, "Denis", "Poltava"));
-            Customers.Add(new Customer(4, "Anna", "Kharkiv"));
-            Customers.Add(new Customer(5, "Evgen", "Kyiv"));
-            Customers.Add(new Customer(6, "Taras", "Kramatorsk"));
-            Customers.Add(new Customer(7, "Stanislav", "Oleksandriya"));
-            Customers.Add(new Customer(8, "Alex", "Lviv"));
-            Customers.Add(new Customer(9, "Anton", "Kyiv"));
+            Customers.Add(new Customer(1, "David", 18, "Lviv"));
+            Customers.Add(new Customer(2, "Boris", 21, "Kyiv"));
+            Customers.Add(new Customer(3, "Denis", 33, "Poltava"));
+            Customers.Add(new Customer(4, "Anna", 17, "Kharkiv"));
+            Customers.Add(new Customer(5, "Evgen", 16, "Kyiv"));
+            Customers.Add(new Customer(6, "Taras", 35, "Kramatorsk"));
+            Customers.Add(new Customer(7, "Stanislav", 31, "Oleksandriya"));
+            Customers.Add(new Customer(8, "Alex", 22, "Lviv"));
+            Customers.Add(new Customer(9, "Anton", 18, "Kyiv"));
         }
 
         private void AddOrder()
         {
             Orders.Add(new Order(10312, 5, 13));
             Orders.Add(new Order(10313, 4, 2));
-            Orders.Add(new Order(10314, 1, 5));
+            Orders.Add(new Order(10314, 3, 15));
             Orders.Add(new Order(10315, 8, 21));
             Orders.Add(new Order(10316, 9, 1));
             Orders.Add(new Order(10317, 2, 3));
             Orders.Add(new Order(10318, 3, 14));
-            Orders.Add(new Order(10319, 7, 5));
+            Orders.Add(new Order(10319, 3, 2));
             Orders.Add(new Order(10320, 6, 18));
             Orders.Add(new Order(10321, 1, 2));
         }
